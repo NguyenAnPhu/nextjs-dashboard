e11fff07c082fe92c85b44a13da6624aba831c9c
Nguyen An Phu <anphu12t2@gmail.com>
2026-08-02 23:59:36 +0700

    update query, pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,17 +4,41 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  // Reset to page 1 when query changes
+  const router = useRouter();
+  useEffect(() => {
+    const queryParam = searchParams.get('query') ?? '';
+    console.log("queryParam: ", queryParam);
+    if (queryParam && currentPage !== 1) {
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+      router.replace(`${pathname}?${params.toString()}`);
+    }
+  }, [searchParams.get('query')]);
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -47,7 +71,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
