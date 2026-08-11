54375b216415d49ff3ad983a0d44f6c8bfe4bfff
Nguyen An Phu <anphu12t2@gmail.com>
2026-08-11 15:10:30 +0700

    update meta, fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,46 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
 import { Metadata } from 'next';
-
+ 
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
-  description: 'The official Next.js Course Dashboard, built with App Router.',
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
   icons: {
-    icon: '/favicon.ico',
+    icon: [
+      { url: '/icon/favicon.ico' },
+      { url: '/icon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/icon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/icon/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/icon/android-icon-192x192.png', sizes: '192x192', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/icon/apple-icon.png' },
+      { url: '/icon/apple-icon-57x57.png', sizes: '57x57', type: 'image/png' },
+      { url: '/icon/apple-icon-60x60.png', sizes: '60x60', type: 'image/png' },
+      { url: '/icon/apple-icon-72x72.png', sizes: '72x72', type: 'image/png' },
+      { url: '/icon/apple-icon-76x76.png', sizes: '76x76', type: 'image/png' },
+      { url: '/icon/apple-icon-114x114.png', sizes: '114x114', type: 'image/png' },
+      { url: '/icon/apple-icon-120x120.png', sizes: '120x120', type: 'image/png' },
+      { url: '/icon/apple-icon-144x144.png', sizes: '144x144', type: 'image/png' },
+      { url: '/icon/apple-icon-152x152.png', sizes: '152x152', type: 'image/png' },
+      { url: '/icon/apple-icon-180x180.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/icon/apple-icon-precomposed.png',
+      },
+    ],
+  },
+  manifest: '/icon/manifest.json',
+  other: {
+    'msapplication-TileColor': '#ffffff',
+    'msapplication-TileImage': '/icon/ms-icon-144x144.png',
+    'msapplication-config': '/icon/browserconfig.xml',
   },
 };
 
